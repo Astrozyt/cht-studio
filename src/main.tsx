39812bdb3a8e219167e67f52bafadcd6c27a77f5
Astrozyt <47319614+Astrozyt@users.noreply.github.com>
2025-08-04 18:52:24 +0200

    Provide placeholders in main

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,12 @@ import Project from "./routes/Project";
 import AppLayout from "./components/AppLyout";
 import Emulator from "./components/projects/Emulator";
 import Projects from "./routes/Projects";
-import { XmlConnector, XmlEditor, XmlOverview } from "./components/projects/Formbuilder/xmlSnippets";
+// import { XmlConnector, XmlOverview } from "./components/projects/Formbuilder_old/xmlSnippets";
+import { FormEditorPage } from "./components/projects/FormEditorPage";
+// import { FormEditor } from "@ght/formbuilder"
+
+const XmlConnector = () => <div>Xml Connector Placeholder</div>;
+const XmlOverview = () => <div>Xml Overview Placeholder</div>;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -17,7 +22,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:projectName" element={<Project />} />
           <Route path="/projects/:projectName/forms" element={<XmlOverview />} />
-          <Route path="/projects/:projectName/forms/:formName" element={<XmlEditor />} />
+          <Route path="/projects/:projectName/forms/:formName" element={<FormEditorPage />} />
           <Route path="/projects/:projectName/tasks/" element={<XmlConnector />} />
           <Route path="/projects/:projectName/emulate" element={<Emulator />} />
           <Route path="/projects/:projectName/overview" element={<Project />} />
